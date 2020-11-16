f1de92fedb63e10816a69e1ccde81e401b4a57d7
MatiasKala <matiraffi@gmail.com>
2020-11-16 00:03:55 -0300

    pasrAEnTransito.js created (-passing items to function lacks)

--- Tp_TransportesRaffi/wwwroot/js/viajes/pasarAEnTransito.js
+++ Tp_TransportesRaffi/wwwroot/js/viajes/pasarAEnTransito.js
@@ -1,3 +1,6 @@
 function pasarAEnTransito() {
-
+    if (window.confirm("Esta seguro de que desea pasar los viajes del dia de hoy a 'En transito'?")) {
+        //cambio
+        alert("Listo!")
+    }
 }
